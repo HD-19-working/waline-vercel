9a29014a9d92dad4b004042b7892a687e08ccb6a
Megamind <megamind3947@163.com>
2024-09-17 14:46:07 +0800

    独立部署

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
+// app.js
 const dotenv = require('dotenv'); // 引入dotenv
+const Waline = require('@waline/vercel');// 引入并执行该文件
+const http = require('http');
+
 dotenv.config(); // 调用config方法合并.env环境变量
-require('@waline/vercel/vanilla.js'); // 引入并执行该文件
+
+const walineConfig = {
+  preSave(comment) {
+      try {
+        if(comment.comment.length > 600 || comment.nick.length > 12 || comment.mail.length > 100 || comment.link.length > 300 ||             comment.ua.length > 300) {
+          return { errmsg: 'Excessive content' };
+        }
+      } catch(e) {
+          
+      }
+  },
+  avatarUrl(comment) {
+    return comment.link;
+  }
+}
+
+http.createServer(Waline(walineConfig)).listen(80, () => {
+  console.log('Server is listening on port 80');
+});
